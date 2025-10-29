ae7c90706c962b06b0fd9e5a9b0c7bdfc58ef086
hactoer321 <boraoncu@gmail.com>
2025-10-29 15:21:51 +0300

    Create route.ts

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import Discord from "next-auth/providers/discord";
+
+const handler = NextAuth({
+  providers: [
+    Discord({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: { strategy: "jwt" },
+});
+export { handler as GET, handler as POST };
