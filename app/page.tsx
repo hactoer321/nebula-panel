464cae22f4e0868c8beb92f8c5829a0c42f829fd
hactoer321 <boraoncu@gmail.com>
2025-10-29 16:38:08 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
-
 export default function Home() {
   return (
     <main style={{padding:20}}>
       <h1>Nebula Panel</h1>
-      <p>Discord ile giriş yap:</p>
-      <Link href="/api/auth/signin">Giriş Yap</Link>
-      <p style={{marginTop:16}}><a href="/dashboard">Dashboard</a></p>
+      <Link href="/api/auth/signin">Discord ile giriş</Link>
+      <p style={{marginTop:12}}><a href="/dashboard">Dashboard</a></p>
     </main>
   );
 }
